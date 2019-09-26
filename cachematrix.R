70a30503f18a75f9f4009ca380c7faa7011a7c7f
Ksprout94 <kylerussell5157@gmail.com>
2019-09-26 19:32:40 -0400

    updated R Program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program calculates the inverse of a matrix, unless it was already calculated
+## in which case, this program will store and be able to find the last matrix
+## inversion done using lexical scoping. This program is a variation of the provided
+## function given by Roger Peng
 
-## Write a short comment describing this function
+## This function creates a vector to get and store the inverse of a matrix
+## so as not to have to repeatedly calculate the same inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(invrs) m <<- invrs
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function solves the inverse of a matrix, unless it has been calculated
+## in which case it finds the matrix inverse in a cached variable
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
